b0ba10251ec9b9f0690b694908b0bd6cbed2a9db
aman vsihwakarma <vishwakarmarambhawan576@gmail.com>
2024-06-22 12:14:40 +0530

    database connected successfully

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDb from './db/index.js'
 
 dotenv.config({
     path: './.env'
 })
+
+connectDb()
 
 const app = express()
 
