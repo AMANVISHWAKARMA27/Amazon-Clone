74ebb189baf39c7f9112aa8a913f6b9c2d898ee8
aman vsihwakarma <vishwakarmarambhawan576@gmail.com>
2024-06-23 15:47:37 +0530

    proxy settings done

--- client/src/components/redux/actions/action.js
+++ client/src/components/redux/actions/action.js
@@ -1,6 +1,6 @@
 export const getProducts = () => async (dispatch) => {
     try {
-        const data = await fetch("http://localhost:8000/getproducts", {
+        const data = await fetch("/getproducts", {
             method:"GET",
             headers:{
                 "content-Type":"application/json"
