47f64b5d21060c1340d6b914383c9b155acad1ca
aman vsihwakarma <vishwakarmarambhawan576@gmail.com>
2024-06-23 15:40:35 +0530

    daa can be fetched from the backend now

--- client/src/components/home/Maincomponent.jsx
+++ client/src/components/home/Maincomponent.jsx
@@ -24,7 +24,7 @@ function Maincomponent() {
       </div>
       <div className='slide_part'>
         <div className='left_slide'>
-          <Slide title="Deal Of The Day" />
+          <Slide title="Deal Of The Day" products={products}/>
         </div>
         <div className='right_slide'>
           <h4>Festive latest launches</h4>
@@ -32,13 +32,13 @@ function Maincomponent() {
           <a href='#'>See More</a>
         </div>
       </div>
-      <Slide title="Today's Deal" />
+      <Slide title="Today's Deal" products={products} />
       <div className='center_img'>
         <img src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg" alt="" />
 
       </div>
-      <Slide title="Best Seller" />
-      <Slide title="Upto 80% Off" />
+      <Slide title="Best Seller" products={products} />
+      <Slide title="Upto 80% Off" products={products} />
     </div>
   )
 }
--- client/src/components/home/Slide.jsx
+++ client/src/components/home/Slide.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import "./slide.css"
+import React from 'react';
+import "./slide.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Divider } from '@mui/material';
-import { products } from './productdata';
 
 const responsive = {
   superLargeDesktop: {
@@ -24,8 +23,7 @@ const responsive = {
   }
 };
 
-
-function Slide({title}) {
+function Slide({ title, products }) {
   return (
     <div className='products_section'>
       <div className='products_deal'>
@@ -33,36 +31,38 @@ function Slide({title}) {
         <button className='view_btn'>View All</button>
       </div>
       <Divider />
-      <Carousel
-        responsive={responsive}
-        infinite={true}
-        draggable={false}
-        swipeable={true}
-        showDots={false}
-        centerMode={true}
-        autoPlay={true}
-        autoPlaySpeed={4000}
-        keyBoardControl={true}
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        dotListClass='custom-dot-list-style'
-        itemClass='carousel-item-padding-40-px'
-        containerClass='carousel-container'
-      >{
-          products.map((e) => {
-            return (
-              <div className='products_items'>
-                <div className='product_img'>
-                  <img src={e.url} alt='' />
-                </div>
-                <p className='products_name'>{e.title.shortTitle}</p>
-                <p className='products_offer'>{e.discount}</p>
-                <p className='products_explore'>{e.tagline}</p>
+      {products ? (
+        <Carousel
+          responsive={responsive}
+          infinite={true}
+          draggable={false}
+          swipeable={true}
+          showDots={false}
+          centerMode={true}
+          autoPlay={true}
+          autoPlaySpeed={4000}
+          keyBoardControl={true}
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          dotListClass='custom-dot-list-style'
+          itemClass='carousel-item-padding-40-px'
+          containerClass='carousel-container'
+        >
+          {products.map((e) => (
+            <div className='products_items' key={e.id}>
+              <div className='product_img'>
+                <img src={e.url} alt='' />
               </div>
-            )
-          })
-        }</Carousel>
+              <p className='products_name'>{e.title.shortTitle}</p>
+              <p className='products_offer'>{e.discount}</p>
+              <p className='products_explore'>{e.tagline}</p>
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        <p>No products available</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default Slide
+export default Slide;
