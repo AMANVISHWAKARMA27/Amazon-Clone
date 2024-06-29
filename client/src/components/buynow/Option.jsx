75c9bcaf077fca0bc02011caa2ed5ee42ddb61d0
Aman Vishwakarma <vishwakarmarambhawan576@gmail.com>
2024-06-29 10:49:35 +0530

    open side on clicking hamburger and menu on profile

--- client/src/components/buynow/Option.jsx
+++ client/src/components/buynow/Option.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useContext } from 'react'
 import "./buynow.css"
 import { LoginContext } from '../context/ContextProvider'
--- client/src/components/header/Navbar.jsx
+++ client/src/components/header/Navbar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./navbar.css"
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import Badge from '@mui/material/Badge';
@@ -7,11 +7,20 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Avatar from '@mui/material/Avatar';
 import { NavLink } from "react-router-dom"
 import { LoginContext } from '../context/ContextProvider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import RightHeader from './RightHeader';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 function Navbar() {
     const { account, setAccount } = useContext(LoginContext)
     console.log(account)
     const cartCount = account?.carts?.length ?? 0;
+
+    const [drawerOpen, setDrawerOpen] = useState(false)
 
     const getValidUserDetail = async () => {
         const res = await fetch("/validuser", {
@@ -25,7 +34,7 @@ function Navbar() {
 
         const data = await res.json()
         console.log(data)
-        if ( res.status !== 201){
+        if (res.status !== 201) {
             console.log('Error')
         } else {
             console.log("Data valid")
@@ -33,14 +42,37 @@ function Navbar() {
         }
     }
 
+    const handleDrawerOpen = () => {
+        setDrawerOpen(true)
+    }
+
+    const handleDrawerClose = () => {
+        setDrawerOpen(false)
+    }
+
     useEffect(() => {
         getValidUserDetail()
-    }, [])
+    })
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <header>
             <nav>
                 <div className='left'>
+                    <IconButton className='hamburgur' onClick={handleDrawerOpen}>
+                        <MenuIcon style={{ color: "#fff" }} />
+                    </IconButton>
+                    <Drawer open={drawerOpen} onClose={handleDrawerClose}>
+                        <RightHeader LogClose={handleDrawerClose} />
+                    </Drawer>
                     <div className='navlogo'>
                         <NavLink
                             to={"/"}>
@@ -79,10 +111,36 @@ function Navbar() {
 
                     </div>
                     {
-                        account ? <Avatar className='avtar2'>
+                        account ? <Avatar className='avtar2'
+                            id="basic-button"
+                            aria-controls={open ? 'basic-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}>
                             {account.name[0].toUpperCase()}
-                        </Avatar> : <Avatar className='avtar'></Avatar>
+                        </Avatar> : <Avatar className='avtar'
+                            id="basic-button"
+                            aria-controls={open ? 'basic-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}></Avatar>
                     }
+
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        {
+                            account ? <MenuItem onClick={handleClose}><LogoutIcon/>&nbsp;Logout</MenuItem> : <NavLink to={"/login"}><MenuItem onClick={handleClose}>SignIn/SignUp</MenuItem></NavLink>
+                        }
+
+                    </Menu>
 
                 </div>
             </nav>
