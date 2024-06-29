773e8c0bf6fbab10c8f88e82ca9550dd41c39f3b
Aman Vishwakarma <vishwakarmarambhawan576@gmail.com>
2024-06-29 22:11:37 +0530

    another deployment link

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser(""))
 app.use(cors({
-    origin:"https://amazon-clone-client-gamma.vercel.app"
+    origin:"https://amazon-clone-client-kcr3muyzm-whilst-somebodys-projects.vercel.app"
 }))
 app.use(router)
 
