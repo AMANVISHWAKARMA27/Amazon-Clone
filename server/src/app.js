fdd47683310a9c115d9421511f39efe702f91b24
Aman Vishwakarma <vishwakarmarambhawan576@gmail.com>
2024-06-30 20:28:13 +0530

    changed configuration

--- server/src/app.js
+++ server/src/app.js
@@ -12,9 +12,9 @@ dotenv.config({
 connectDb()
 
 const app = express()
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser(""))
-app.use(cors());
 app.use(router)
 
 app.listen(process.env.PORT, () => {
